17b30e50abf64a66fcd6be0835675be285e602f2
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-08-17 19:32:55 +0200

    Updated blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -1,15 +1,27 @@
 import json
 
 class Blockchain:
-  
-  def init(self):
-    self.actual_transactions = {}
-    self.blockchain = {}
-    self.blockchainnumber = 0
-    self.blocks = 0
-    self.nodes = set()
     
-  def create_transaction(self, sender, receiver, coins, message):
-    self.previoushash = self.previoushash
-    self.coins = coins
-    self.message = message
+    def init(self):
+        self.actual_transactions = {}
+        self.blockchain = {}
+        self.blockchainnumber = 0
+        self.block = {}
+        self.blocksnumber = 0
+        self.nodes = set()
+    
+    def create_transaction(self, sender, receiver, coins, message):
+        self.previoushash = self.previoushash
+        self.sender = sender
+        self.receiver = receiver
+        self.coins = coins
+        self.message = message
+        self = self ^ privateKey + difficulty
+        
+    def create_block(self, previoushash, transactions, transactionsnumber, totalfees):
+        self.prevhash = previoushash
+        self.txs = transactions
+        self.txsnumber = transactionsnumber
+        self.fees = totalfees
+    
+blockchain = Blockchain
